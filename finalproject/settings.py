5db684e36c8607668f35efdc70da7b612547fbbf
Sophia Pobre <sophiapobre@gmail.com>
2025-09-27 17:13:47 +0800

    Changed local development to use SQLite

--- finalproject/settings.py
+++ finalproject/settings.py
@@ -90,12 +90,8 @@ WSGI_APPLICATION = 'finalproject.wsgi.application'
 if DEBUG:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': env('DATABASE_NAME'),
-            'USER': env('DATABASE_USER'),
-            'PASSWORD':env('DATABASE_PASSWORD'),
-            'HOST': 'localhost',
-            'PORT': '5432',
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 else:
